557c40a026b15fc5a070b65d172bd2174a461b08
Amirhossein Mohammadi <accretence@outlook.com>
2025-01-13 10:26:09 +0330

    fix build issue

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -40,9 +40,6 @@ export function formatDate(input: string | number): string {
   });
 }
 
-export const isOurCdnUrl = (url: string) =>
-  url?.includes("utfs.io") || url?.includes("uploadthing.com");
-
 export const getImageKeyFromUrl = (url: string) => {
   const parts = url.split("/");
   return parts.at(-1);
